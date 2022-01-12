f9830156aa454b163f2733db07c22c75354eb4e9
rasset69@gmail.com <rasset69@gmail.com>
2022-01-11 19:56:25 -0700

    Add Beep Function

--- SethRasFirstAssignment/Hello_World.vb
+++ SethRasFirstAssignment/Hello_World.vb
@@ -2,13 +2,15 @@
 'RCET 0265
 'Spring 2022
 'Assignment 1 Hello_World
-'
+'https://github.com/SethRas/SethRasFirstAssignment.git
 
 Module Hello_World
 
     Sub Main()
         Console.WriteLine("Hello_World_This_Is_Seth")
         Console.ReadLine()
+        Console.Beep()
+
     End Sub
 
 End Module
